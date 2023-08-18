d16511fec2c6dfc2145bd8e135ec9e666e3815c4
baananou <bananoumohamed@live.fr>
2023-08-18 17:21:28 +0100

    auth ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
+import ToasterProvider from './providers/ToasterProvider'
 export const metadata = {
   title: 'Airbnb',
   description: 'Airbnb Clone',
@@ -18,8 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
-        {children}</body>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
   )
 }
